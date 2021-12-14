4db096d8a574763d072bbc2fbec6cdb5444388ed
Relanit <68604445+Relanit@users.noreply.github.com>
2021-12-14 08:43:41 +0300

    Update Transfers.sol

--- Transfers.sol
+++ Transfers.sol
@@ -11,13 +11,22 @@ contract Transfers {
     mapping(address => User) public users;
     mapping(string => address) public addresses;
     
-    uint adminsNum;
+    uint public adminsNum;
     
     constructor() {
-        users[0xe1c99B15bFb7431A2C228B9f1AA7FC44506ED7c8] = User("1", true);
-        addresses["1"] = 0xe1c99B15bFb7431A2C228B9f1AA7FC44506ED7c8;
-        users[0xf7Af9FB9a14823A50D2A27170938a10E7182b68F] = User("2", true);
-        addresses["2"] = 0xf7Af9FB9a14823A50D2A27170938a10E7182b68F;
+         users[0x350E67c63aAE498430DF311680b084A84E3D7C25] = User("1", true);
+        users[0xF5D653C84A39DFE9BFFe6ccBCFA237c4F7C9ce41] = User("2", true);
+        users[0x0e4310516aE847A94aE2dBF71D1681B39e487030] = User("3", false);
+        users[0xA618381cc1eb2344FAeb1518d980AEac02bB312B] = User("4", false);
+        users[0x6Be283ae96a0A01825bDbB9AdC57354FBAd947D9] = User("5", false);
+        users[0xdDDc972610d6767A98aE0333f869f11ea8655362] = User("6", false);
+        addresses["1"] = 0x350E67c63aAE498430DF311680b084A84E3D7C25;
+        addresses["2"] = 0xF5D653C84A39DFE9BFFe6ccBCFA237c4F7C9ce41;
+        addresses["3"] = 0x0e4310516aE847A94aE2dBF71D1681B39e487030;
+        addresses["4"] = 0xA618381cc1eb2344FAeb1518d980AEac02bB312B;
+        addresses["5"] = 0x6Be283ae96a0A01825bDbB9AdC57354FBAd947D9;
+        addresses["6"] = 0xdDDc972610d6767A98aE0333f869f11ea8655362;
+        adminsNum = 2;
         
         categories.push("Lichniy perevod");
         categories.push("Oplata arendy zhilya");
@@ -29,24 +38,22 @@ contract Transfers {
         patterns.push(Pattern("Kvartplata", 1, 7 ether));
         patterns.push(Pattern("Kvartplata", 1, 9 ether));
         patterns.push(Pattern("Pogashenie zadolzhennosti", 2, 10 ether));
-        
-        adminsNum = 2;
     }
     
     function getHash(string memory str) public pure returns(bytes32) {
-        return keccak256(bytes(str));
+        return(keccak256(bytes(str)));
     }
     
     function getAddress(string memory login) public view returns(address) {
-        return addresses[login];
+        return(addresses[login]);
     }
 
     function getBalance(address addr) public view returns(uint) {
-        return addr.balance;
+        return(addr.balance);
     }
     
     function createUser(address addr, string memory login) public {
-        require(msg.sender == 0x0516A279fe30508D857443485a1A2E14a384067e, "Not a zero account");
+        require(msg.sender == 0x3298Dd4C3Ccd949F9F636DDeB31Baf8B9cC7Dc62, "Not a zero account");
         require(addresses[login] == address(0), "Account with this login already exists");
         users[addr] = User(login, false);
         addresses[login] = addr;
@@ -90,13 +97,13 @@ contract Transfers {
         uint categoryId;
         string description;
         uint time;
-        bool finished;
+        bool status;
     }
     
     Transfer[] public transfers;
 
     function getTransferID() public view returns(uint) {
-        return transfers.length - 1;
+        return(transfers.length-1);
     }
     
     function createTransfer(address toAddress, string memory codeword, uint categoryId, string memory description) public payable {
@@ -109,7 +116,7 @@ contract Transfers {
     
     function confirmTransfer(uint transferId, string memory codeword) public payable {
         require(transferId < transfers.length, "Invalid id");
-        require(transfers[transferId].finished == false, "Transfer is already finished");
+        require(transfers[transferId].status, "Transfer is not active");
         require(msg.sender == transfers[transferId].toAddress, "Not for you");
         if (transfers[transferId].codewordHash == getHash(codeword)) {
             payable(msg.sender).transfer(transfers[transferId].value);
@@ -118,19 +125,19 @@ contract Transfers {
         else {
             payable(transfers[transferId].fromAddress).transfer(transfers[transferId].value);
         }
-        transfers[transferId].finished = true;
+        transfers[transferId].status = false;
     }
     
     function cancelTransfer(uint transferId) public payable {
         require(transferId < transfers.length, "Invalid id");
-        require(transfers[transferId].finished == false, "Transfer is already finished");
+        require(transfers[transferId].status, "Transfer is not active");
         require(msg.sender == transfers[transferId].fromAddress, "Not for you");
         payable(msg.sender).transfer(transfers[transferId].value);
-        transfers[transferId].finished = true;
+        transfers[transferId].status = false;
     }
     
     modifier admin() {
-        require(users[msg.sender].admin == true, "Not admin");
+        require(users[msg.sender].admin, "Not admin");
         _;
     }
     
